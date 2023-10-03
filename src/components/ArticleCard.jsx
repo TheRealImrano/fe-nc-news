1403636a98064daad9f7c82d62d1115fba9a3bf5
Ali Imran <real.imrano@proton.me>
2023-10-03 18:40:18 +0100

    completed ticket-7; pushing to merge

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,9 +1,51 @@
+import React, { useState } from 'react';
 import dayjs from 'dayjs';
 import 'dayjs/locale/en';
 import {Link} from 'react-router-dom'
+import { updateArticleVotes } from '../utils/api';
 
 const ArticleCard = ({article}) => {
     const formattedDate = dayjs(article.created_at).locale('en').format('MMMM D, YYYY h:mm A');
+    const [voteStatus, setVoteStatus] = useState(null);
+    const [voteCount, setVoteCount] = useState(article.votes);
+
+    const handleUpvote = () => {
+        if (voteStatus === 'upvoted') {
+          // User is undoing an upvote
+          setVoteStatus(null);
+          setVoteCount((prevCount) => prevCount - 1); // Decrement voteCount locally
+          updateArticleVotes(article.article_id, -1); // Make the API call to update votes
+        } else if (voteStatus === null){
+          // User is upvoting
+          setVoteStatus('upvoted');
+          setVoteCount((prevCount) => prevCount + 1); // Increment voteCount locally
+          updateArticleVotes(article.article_id, 1); // Make the API call to update votes
+        } else if (voteStatus === 'downvoted'){
+          // User is upvoting
+          setVoteStatus('upvoted');
+          setVoteCount((prevCount) => prevCount + 2); // Increment voteCount locally
+          updateArticleVotes(article.article_id, 2); // Make the API call to update votes
+        }
+      };
+      
+      const handleDownvote = () => {
+        if (voteStatus === 'downvoted') {
+          // User is undoing a downvote
+          setVoteStatus(null);
+          setVoteCount((prevCount) => prevCount + 1); // Increment voteCount locally
+          updateArticleVotes(article.article_id, 1); // Make the API call to update votes
+        } else if (voteStatus === null){
+          // User is downvoting
+          setVoteStatus('downvoted');
+          setVoteCount((prevCount) => prevCount - 1); // Decrement voteCount locally
+          updateArticleVotes(article.article_id, -1); // Make the API call to update votes
+        } else if (voteStatus === 'upvoted'){
+          // User is downvoting
+          setVoteStatus('downvoted');
+          setVoteCount((prevCount) => prevCount - 2); // Decrement voteCount locally
+          updateArticleVotes(article.article_id, -2); // Make the API call to update votes
+        }
+      };
 
     return (
         <article className="component-outline">
@@ -16,9 +58,19 @@ const ArticleCard = ({article}) => {
                 <button> comment ({article.comment_count})</button>
             </Link>
             <div>
-            <button>upvote</button>
-            <span>{article.votes}</span>
-            <button>downvote</button>
+            <button
+                onClick={handleUpvote}
+                className={voteStatus === 'upvoted' ? 'upvoted-button' : ''}
+            >
+                upvote
+            </button>
+            <span>{' '}{voteCount}{' '}</span>
+            <button
+                onClick={handleDownvote}
+                className={voteStatus === 'downvoted' ? 'downvoted-button' : ''}
+            >
+                downvote
+            </button>
             </div>
             <Link to={`/articles/${article.article_id}`}>
                 <h4>{article.title}</h4>
